4af09f4cfbfa6d9da6ff5558f13866e3c3e52f05
Ouss84 <bahrioussama84@gmail.com>
2021-12-09 13:28:15 +0200

    intializing the grid

---
+++ src/Maze.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { Grid, Paper, Box } from "@material-ui/core";
+import Cell from "./Cell";
+
+class Maze extends Component {
+  matrix = [2];
+  paths = [];
+  idx = 0;
+  numPaths = 0;
+  pathIdx = 0;
+
+  index = () => {
+    this.idx += 1;
+    return this.matrix[this.idx];
+  };
+  render() {
+    return (
+      <div>
+        <h1> Rat in a maze </h1>
+        <Grid container spacing={2} justify="center" direction="column"></Grid>
+        <p>
+          <Grid container spacing={1} justify="center" direction="row">
+            <Grid item>
+              <Paper elevation={3}>
+                <Box padding={2} height={50} width={50}>
+                  <center></center>
+                </Box>
+              </Paper>
+            </Grid>
+            <Cell N={this.index()}></Cell>
+            <Cell N={this.index()}></Cell>
+            <Cell N={this.index()}></Cell>
+          </Grid>
+        </p>
+        <p>
+          <Grid container spacing={1} justify="center" direction="row">
+            <Cell N={this.index()}></Cell>
+            <Cell N={this.index()}></Cell>
+            <Cell N={this.index()}></Cell>
+            <Cell N={this.index()}></Cell>
+          </Grid>
+        </p>
+        <p>
+          <Grid container spacing={1} justify="center" direction="row">
+            <Cell N={this.index()}></Cell>
+            <Cell N={this.index()}></Cell>
+            <Cell N={this.index()}></Cell>
+            <Cell N={this.index()}></Cell>
+          </Grid>
+        </p>
+        <p>
+          <Grid container spacing={1} justify="center" direction="row">
+            <Grid item>
+              <Paper elevation={3}>
+                <Box padding={2} height={50} width={50}>
+                  <center></center>
+                </Box>
+              </Paper>
+            </Grid>
+            <Cell></Cell>
+            <Cell></Cell>
+            <Cell></Cell>
+          </Grid>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Maze;
